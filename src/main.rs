9dc1d9ebf33dd4a0cf97d911f9343d0f90f1e18f
M. Bossert <no@valid.email>
2021-09-02 04:02:20 +0200

    Implement randomization of the code. This also changes the lastcode format. Previous progress will be rendered invalid.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use rand::prelude::*;
+use std::char::from_digit;
 use std::env;
 use std::fs::File;
-use std::io::{Error, Read, Write};
+use std::io::{Error, ErrorKind, Read, Write};
 use std::process::Command;
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::Ordering::{Acquire, SeqCst};
@@ -10,15 +12,25 @@ use std::time::Instant;
 fn main() {
     let total_time = Instant::now();
     let total_time_ctrlc = Instant::now();
-    let base_start: Arc<AtomicU64> = Arc::new(AtomicU64::new(1000000000000000));
 
-    handle_args(env::args().collect(), base_start.clone());
+    let base_start: Arc<AtomicU64> = Arc::new(AtomicU64::new(1000000000000000));
+    let mut randomizer = Vec::new();
+
+    handle_args(env::args().collect(), base_start.clone(), &mut randomizer);
 
     let cd_num = base_start.clone();
+    let cd_rnd = randomizer.clone();
     ctrlc::set_handler(move || {
         println!("Received Ctrl-C.");
-        print!("Saving current code {} ...", cd_num.load(Acquire));
-        match saver(cd_num.load(Acquire)) {
+        print!(
+            "Saving current code {} ({}) ...",
+            cd_num.load(Acquire),
+            cd_rnd
+                .iter()
+                .map(|d| from_digit(*d as u32, 16).unwrap())
+                .collect::<String>()
+        );
+        match saver(cd_num.load(Acquire), &cd_rnd) {
             Ok(_) => println!(" successful."),
             Err(e) => println!(" failed: {:?}", e),
         }
@@ -32,8 +44,19 @@ fn main() {
     let mut stdout: Option<[String; 2]> = None;
     let mut before = Instant::now();
 
+    // main loop
     loop {
-        let code = base_start.load(Acquire).to_string();
+        let mut code = vec!['0'; 16];
+        for (c, t) in base_start
+            .load(Acquire)
+            .to_string()
+            .chars()
+            .zip(randomizer.iter())
+        {
+            code[*t as usize] = c;
+        }
+        let code = code.iter().collect::<String>();
+
         let output = Command::new("/usr/bin/fastboot")
             .args(&["oem", "unlock"])
             .arg(&code)
@@ -53,8 +76,9 @@ fn main() {
 
         let new_instant = Instant::now();
         println!(
-            "code: {}, {}, stdout: {}, stderr: {}, elapsed: {}ms",
+            "code: {} ({}), {}, stdout: {}, stderr: {}, elapsed: {}ms",
             code,
+            base_start.load(Acquire),
             output.status,
             o_s[0],
             o_s[1],
@@ -81,7 +105,7 @@ fn main() {
     }
 
     print!("Storing last used code {} ... ", base_start.load(Acquire));
-    match saver(base_start.load(Acquire)) {
+    match saver(base_start.load(Acquire), &randomizer) {
         Ok(_) => println!("success."),
         Err(e) => println!("failed: {}", e),
     }
@@ -89,46 +113,129 @@ fn main() {
     println!("Current code: {}", base_start.load(Acquire));
 }
 
-fn handle_args(args: Vec<String>, base_start: Arc<AtomicU64>) {
+fn handle_args(args: Vec<String>, base_start: Arc<AtomicU64>, randomizer: &mut Vec<u8>) {
     if args.len() > 1 {
+        // check first argument / code to start with
         match args[1].trim_end().parse::<u64>() {
             Ok(prev_start) => base_start.store(prev_start, SeqCst),
             Err(_) => help_text(),
         }
+        // check whether our code is exactly 16 digits long
         if base_start.load(Acquire).to_string().len() != 16 {
             println!("Error. Code is not exactly 16 digits long.\n");
             help_text();
         }
-        println!("Loaded offset {:?} successfully. Resuming.", base_start);
-    } else {
-        if resumer(base_start.clone()).is_ok() {
+
+        // check second argument / randomizing string
+        if args[2].chars().any(|c| !c.is_ascii_hexdigit()) {
             println!(
-                "Loaded offset {:?} successfully from 'lastcode'. Resuming.",
-                base_start
+                "Error. Randomizer does not consist exclusively of hex digits: {}",
+                args[2]
             );
+            help_text();
         }
+        randomizer.extend(
+            args[2]
+                .chars()
+                .map(|c| u8::from_str_radix(&c.to_string(), 16).unwrap()),
+        );
+        if check_randomizer(&randomizer).is_err() {
+            help_text();
+        }
+        println!(
+            "Loaded offset {:?}, randomizer {} successfully. Resuming.",
+            args[1], args[2]
+        );
+    } else {
+        if resumer(base_start.clone(), randomizer).is_ok() {
+            println!(
+                "Loaded offset {:?}, randomizer {} successfully from 'lastcode'. Resuming.",
+                base_start,
+                randomizer
+                    .iter()
+                    .map(|b| from_digit(*b as u32, 16).unwrap())
+                    .collect::<String>()
+            );
+        } else {
+            base_start.store(1_000_000_000_000_000, SeqCst);
+            *randomizer = (1..=0xf).collect::<Vec<_>>();
+            // need to randomize
+            let mut rng = rand::thread_rng();
+            randomizer.shuffle(&mut rng);
+            randomizer.insert(0, 0);
+
+            if check_randomizer(&randomizer).is_err() {
+                panic!("randomizer initialization is faulty!");
+            }
+        }
+    }
+
+    if check_randomizer(&randomizer).is_err() {
+        panic!("randomizer is faulty!");
     }
 }
 
-fn resumer(base_start: Arc<AtomicU64>) -> std::io::Result<()> {
+fn check_randomizer(randomizer: &Vec<u8>) -> std::io::Result<()> {
+    let mut rand_test = randomizer.clone();
+    rand_test.sort_unstable();
+
+    if rand_test.len() != 16 || // not 16 digits
+        (0..=0xf).zip(rand_test.iter()).any(|(a,b)| a != *b) || // not all digits present
+        (0..=0xf).zip(randomizer.iter()).all(|(a,b)| a == *b)
+    // not rnd
+    {
+        Err(Error::new(ErrorKind::InvalidData, "randomizer is invalid"))
+    } else {
+        Ok(())
+    }
+}
+
+fn resumer(base_start: Arc<AtomicU64>, randomizer: &mut Vec<u8>) -> std::io::Result<()> {
     let mut f = File::open("lastcode")?;
     let mut buffer = String::new();
 
     f.read_to_string(&mut buffer)?;
     base_start.store(
         buffer
-            .trim_end()
+            .chars()
+            .take(16)
+            .collect::<String>()
             .parse::<u64>()
-            .map_err(|e| Error::new(std::io::ErrorKind::InvalidData, e.to_string()))?,
+            .map_err(|e| Error::new(ErrorKind::InvalidData, e.to_string()))?,
         SeqCst,
     );
+
+    if buffer
+        .chars()
+        .skip(16)
+        .take(16)
+        .any(|c| !c.is_ascii_hexdigit())
+    {
+        return Err(Error::new(ErrorKind::InvalidData, "not all hexdigits"));
+    }
+    *randomizer = buffer
+        .chars()
+        .skip(16)
+        .take(16)
+        .map(|c| u8::from_str_radix(&c.to_string(), 16).unwrap())
+        .collect::<Vec<_>>();
+    check_randomizer(&randomizer)?;
 
     Ok(())
 }
 
-fn saver(base_start: u64) -> std::io::Result<()> {
+fn saver(base_start: u64, randomizer: &Vec<u8>) -> std::io::Result<()> {
     let mut f = File::create("lastcode")?;
-    f.write_all(base_start.to_string().as_bytes())?;
+    let mut savestring = base_start.to_string();
+    while savestring.len() != 16 {
+        savestring.insert(0, '0');
+    }
+    savestring.extend(
+        randomizer
+            .iter()
+            .map(|d| from_digit(*d as u32, 16).unwrap()),
+    );
+    f.write_all(savestring.as_bytes())?;
 
     Ok(())
 }
@@ -137,8 +244,6 @@ fn help_text() {
     println!("This tool is supposed to unlock a Huawei MediaPad M3.");
     println!("Run it with exactly 1 argument to start with an offset other than 1000000000000000.");
     println!("Else it tries to load the previously used offset from 'lastcode' in $PWD.");
-    println!();
-    println!("Fuck Huawei for leaving us out in the rain.");
 
     std::process::exit(0);
 }
